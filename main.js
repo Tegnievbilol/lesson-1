5ec81aea2f4b677f4d9dbbb92ecc25c657fc6381
Tegnievbilol <tegniev@gmail.com>
2024-04-17 16:10:27 +0500

    lesson-1 commit

---
+++ main.js
@@ -0,0 +1,35 @@
+// Task-1
+// function sum(a,b){
+//     return a+b
+// }
+// console.log(sum(2,3));
+
+
+// Task-2
+// function sum(a){
+//     return a>0 ? "Position" : a<0 ? "Negative" : "Zero"
+// }
+// console.log(sum(8));
+
+
+// Task-3
+// function sum(a){
+//     for(let i=0;i<=10;i++){
+//         console.log(a+" * "+i+" = "+a*i);
+//     }
+// }
+// console.log(sum(5));
+
+
+// Task-4
+// function sum(a){
+//     return a%10==9 ? "Yes" : "No"
+// }
+// console.log(sum(128));
+
+
+// Task-5
+// function sum(a){
+//     return Math.ceil(a/100)
+// }
+// console.log(sum(2200));
